5ce52d2ddd5382b1b247bc70b2512310af9d4975
Isibor Joseph <softjoe40@gmail.com>
2023-03-22 13:17:46 -0700

    made changes to main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef main.h
-#define main.h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,5 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_t0_98(int n);
 void print_times_table(int n);
-
-#endif
