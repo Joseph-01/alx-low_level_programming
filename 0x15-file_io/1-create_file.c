e9d003fef51827d4e4421e672fe34c9e4d903ef0
Joseph-01 <softjoe40@gmail.com>
2023-12-22 14:54:30 +0100

    working 1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, result = 1, countn = 0;
+	
+	if(!filename)
+		return (-1);
+	
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if(file == -1)
+		return (-1);
+	
+	if(text_content)
+	{
+		while(text_content[countn]){
+			countn++;
+			result = write(file, text_content, countn);
+		}
+	
+	}
+	
+	if(result == -1)
+		return (-1);
+	
+	close(file);
+	return (1);
+
+}
