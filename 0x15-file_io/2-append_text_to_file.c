155e50a30c51065d43869511f295c0806d16d00b
Joseph-01 <softjoe40@gmail.com>
2023-12-22 21:10:58 +0100

    working on append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+    if (filename == NULL)
+    {
+        return -1;
+    }
+
+    if (text_content == NULL)
+    {
+        return 1;
+    }
+
+    FILE *file = fopen(filename, "a");
+    if (file == NULL)
+    {
+        return -1;
+    }
+
+    if (fputs(text_content, file) == EOF)
+    {
+        fclose(file);
+        return -1;
+    }
+
+    fclose(file);
+
+    return 1;
+}
