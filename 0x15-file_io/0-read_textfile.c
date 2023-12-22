7b936147828e2ed3bc7cda0fa09fd02128db32f1
Joseph-01 <softjoe40@gmail.com>
2023-12-22 02:01:18 +0100

    working on 0-read_textfile task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,28 +3,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t bytesRead;
-    char buffer[500];
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
+    FILE *pf = fopen(filename, "r");
+    char *buffer;
+    buffer = malloc(sizeof(char) * letters);
+    if (pf == NULL)
     {
-        /* Handle error opening the file*/
-        perror("Error opening file");
-        return -1; 
+        printf("File does not exist");
+    }
+    else
+    {
+        while (fgets(buffer, 200, pf) != NULL)
+        {
+            printf("%s", buffer);
+        }
+        fclose(pf);
     }
 
-    bytesRead = read(fd, buffer, letters);
-
-    if (bytesRead == -1)
-    {
-        /* Handle error reading from the file*/
-        perror("Error reading file");
-        close(fd);
-        return -1; /* or another error indicator*/
-    }
-
-    /* Process the data in the buffer or do something with bytesRead*/
-
-    close(fd);
-    return bytesRead; /* Number of bytes read*/
+    printf("\n");
+    return (0);
 }
