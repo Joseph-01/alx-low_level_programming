cedd5fb14ae3e03061557418a7bd7fc914de70a8
Joseph-01 <softjoe40@gmail.com>
2023-12-22 01:42:23 +0100

    working on 0-read_textfile task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        // Handle error opening the file
+        perror("Error opening file");
+        return -1; // or another error indicator
+    }
+
+    char buffer[letters];
+    ssize_t bytesRead = read(fd, buffer, letters);
+
+    if (bytesRead == -1)
+    {
+        // Handle error reading from the file
+        perror("Error reading file");
+        close(fd);
+        return -1; // or another error indicator
+    }
+
+    // Process the data in the buffer or do something with bytesRead
+
+    close(fd);
+    return bytesRead; // Number of bytes read
+}
