14d4618832502aac0b98742a3b2a162d3a27df1b
Joseph <softjoe40@gmail.com>
2023-12-22 12:18:50 -0800

    about to check append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,6 +3,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+    FILE *file;
 
     if (filename == NULL)
     {
@@ -14,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
         return 1;
     }
 
-    FILE *file = fopen(filename, "a");
+    file = fopen(filename, "a");
     if (file == NULL)
     {
         return -1;
