bd5e9c123405f6da3d08d3f4631a5aa5e8a0b640
Joseph <softjoe40@gmail.com>
2023-12-21 17:06:05 -0800

    about to check read testfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     buffer = malloc(sizeof(char) * letters);
     if (pf == NULL)
     {
-        return (0)
+        return (0);
     }
     else
     {
