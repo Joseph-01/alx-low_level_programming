3b097a73983d939db7f89689ecc146c9c150aefc
Joseph-01 <softjoe40@gmail.com>
2023-12-22 09:22:18 +0100

    working on 0-read_textfile task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,23 +3,45 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *pf = fopen(filename, "r");
+    int fd;
+    ssize_t _read, _write;
     char *buffer;
-    buffer = malloc(sizeof(char) * letters);
-    if (pf == NULL)
+
+    if (!filename)
     {
         return (0);
     }
-    else
+
+    /*open file*/
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
     {
-        while (fgets(buffer, 200, pf) != NULL)
-        {
-            printf("%s", buffer);
-        }
-        free(buffer);
-        fclose(pf);
+        return (0);
     }
 
-    printf("\n");
-    return (0);
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        return (0);
+    }
+
+    _read = read(fd, buffer, letters);
+    if (_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    /*write*/
+    _write = write(STDOUT_FILENO, buffer, _read);
+    if (_write == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    close(fd);
+    return (_read);
 }
