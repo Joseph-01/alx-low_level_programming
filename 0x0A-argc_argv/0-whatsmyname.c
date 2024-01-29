cc1574d467e8ec16c4addb89e22987971d93c035
Joseph-01 <softjoe40@gmail.com>
2024-01-29 13:53:49 -0800

    wrote script to print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: argument vecto
+ *
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
-  printf("%s \n", argv[0]);
-  return (argc);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
 }
