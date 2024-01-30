a02185df74f151d8b7b32561c4ad4d3e957884e5
Joseph-01 <softjoe40@gmail.com>
2024-01-30 00:45:29 -0800

    script to print all arguments passed to out program

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
 
 int main(int argc, char *argv[])
 {
-int count;
-for (count = 0; count < argc; count++)
-{
-printf("%s\n", argv[count]);
-}
-return (0);
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
