ba8624baba6623f55ee2814a23207828aa039fdf
Joseph-01 <softjoe40@gmail.com>
2024-01-31 02:41:42 -0800

    4. To infinity and beyond
    c script to add numbers in the command line
    by passing them as argument to an executable/complie c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int i, j;
+    if (argc < 2) {
+        printf("0\n");
+        return 0;
+    }
+    
+    for (i = 1; i < argc; i++) {
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        
+        sum += atoi(argv[i]);
+    }
+    
+    printf("%d\n", sum);
+    return 0;
+}
