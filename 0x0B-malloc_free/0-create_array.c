a4258cae3c64606bbc34ef5b70300cb8ef140cdd
Joseph-01 <softjoe40@gmail.com>
2024-02-02 14:22:29 -0800

    0. Float like a butterfly, sting like a bee
    A function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+*@create_array cretes array of size t
+*@params size: size of array
+*@params char c: character to add to array
+*/
 char *create_array(unsigned int size, char c)
 {
- unsigned int i;
- char *ptr;
- ptr = (char *)malloc(sizeof(char) * size);
- for (i=0; i<size; i++) {
-  ptr[i] = c; 
- }
- return (ptr); 
+unsigned int i;
+char *ptr;
+ptr = (char *)malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+ptr[i] = c;
+}
+printf("\n");
+return (ptr);
 }
