b94f9fe0a97e7968c05e2554fe753a0adf991d29
Joseph-01 <softjoe40@gmail.com>
2024-02-02 14:35:58 -0800

    create readme file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *ptr;
 ptr = (char *)malloc(sizeof(char) * size);
-for (i = 1; i < size; i++)
+for (i = 0; i < size; i++)
 {
 ptr[i] = c;
 }
